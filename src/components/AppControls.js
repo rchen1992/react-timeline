a7d3b421a061b0fe220b1381c1cad37d2b24b8d4
Raymond Chen <rchen1992@gmail.com>
2019-06-04 22:14:28 -0700

    Add current year to header.

--- src/components/AppControls.js
+++ src/components/AppControls.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Header from './Header';
 import { DatePicker } from 'antd';
 import { StateContext } from 'store';
 
+const Year = styled.span`
+    font-weight: bold;
+    margin-right: 10px;
+`;
+
 function AppControls(props) {
-    const { setYear, setSelectedDay } = React.useContext(StateContext);
+    const { year, setYear, setSelectedDay } = React.useContext(StateContext);
     function onDateChange(date) {
         if (date) {
             setYear(date.year());
@@ -14,6 +20,7 @@ function AppControls(props) {
 
     return (
         <Header>
+            <Year>{year}</Year>
             <DatePicker placeholder="Jump to date" onChange={onDateChange} />
         </Header>
     );
