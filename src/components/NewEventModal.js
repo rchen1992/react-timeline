d502dc38f159a8f653a91288a40093335ea96936
Raymond Chen <rchen1992@gmail.com>
2019-06-05 22:53:52 -0700

    Add react color picker to new event modal.

--- src/components/NewEventModal.js
+++ src/components/NewEventModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Modal, Input, DatePicker } from 'antd';
+import { CirclePicker } from 'react-color';
 
 const { TextArea } = Input;
 const { RangePicker } = DatePicker;
@@ -12,25 +13,82 @@ const Label = styled.label`
 `;
 
 const Field = styled.div`
-    margin-bottom: 10px;
+    margin-bottom: 22px;
 `;
 
+const colors = [
+    '#f44336',
+    '#e91e63',
+    '#9c27b0',
+    '#673ab7',
+    '#3f51b5',
+    '#2196f3',
+    '#03a9f4',
+    '#00bcd4',
+    '#009688',
+    '#4caf50',
+    '#8bc34a',
+    '#cddc39',
+    '#ffeb3b',
+    '#ffc107',
+    '#ff9800',
+    '#ff5722',
+    '#795548',
+    '#607d8b',
+];
+
 function NewEventModal(props) {
+    const [color, setColor] = React.useState(colors[0]);
+    const [name, setName] = React.useState('');
+    const [startDate, setStartDate] = React.useState(null);
+    const [endDate, setEndDate] = React.useState(null);
+
+    function onColorChange(color) {
+        setColor(color.hex);
+    }
+
+    function onNameChange(e) {
+        setName(e.target.value);
+    }
+
+    function onDateChange(dates) {
+        setStartDate(dates[0]);
+        setEndDate(dates[1]);
+    }
+
+    function onClose() {
+        setColor(colors[0]);
+        setName('');
+        setStartDate(null);
+        setEndDate(null);
+
+        props.onClose();
+    }
+
     return (
         <Modal
             title="New Event"
             visible={props.open}
-            onOk={props.onClose}
-            onCancel={props.onClose}
+            onOk={onClose}
+            onCancel={onClose}
             okText="Submit"
         >
             <Field>
-                <Label htmlFor="event-name-input">Event Description:</Label>
-                <TextArea id="event-name-input" rows={2} placeholder="Enter event description" />
+                <Label htmlFor="event-name-input">Event Name:</Label>
+                <TextArea id="event-name-input" rows={2} value={name} onChange={onNameChange} />
             </Field>
             <Field>
-                <Label htmlFor="event-name-input">Event Dates:</Label>
-                <RangePicker style={{ width: '100%' }} />
+                <Label>Event Dates:</Label>
+                {startDate && startDate.format('YYYY-MM-DD')}
+                <RangePicker
+                    style={{ width: '100%' }}
+                    onChange={onDateChange}
+                    value={[startDate, endDate]}
+                />
+            </Field>
+            <Field>
+                <Label>Event Color:</Label>
+                <CirclePicker width="100%" color={color} colors={colors} onChange={onColorChange} />
             </Field>
         </Modal>
     );
